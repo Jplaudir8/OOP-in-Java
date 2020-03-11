d45a77566b3bc04501bf9b8b9433c83d90e61432
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-03-11 12:16:00 -0500

    Finished twoOcurrences method

--- DNA Exercises/StringsFirstAssigments/Part3.java
+++ DNA Exercises/StringsFirstAssigments/Part3.java
@@ -7,15 +7,14 @@
 public class Part3 {
     public boolean twoOcurrences(String stringa, String stringb) {
         int c = 0;
-        int startIndex = 0;
-        
-        for(int i = 0; i < stringb.length(); i++){
-            if(stringb.indexOf(stringa, startIndex) != -1){
-                startIndex = stringb.indexOf(stringa, startIndex + 1);
-                c = c + 1;
-            }
+        int startIndex = stringb.indexOf(stringa);
+
+        while(startIndex != -1){
+            startIndex = stringb.indexOf(stringa, startIndex + stringa.length() + 1);
+            c++;
         }
-        if (c >= 2){
+        System.out.println("Counter: " + c);
+        if (c >= 2) {
             return true;
         } else {
             return false;
@@ -31,6 +30,7 @@ public class Part3 {
         boolean result = twoOcurrences(stra, strb);
         System.out.println("Result: " + result);
         System.out.println("");
+        
         
         stra = "a";
         strb = "banana";
