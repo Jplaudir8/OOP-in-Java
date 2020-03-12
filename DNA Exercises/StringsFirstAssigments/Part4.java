857433932c8d893e3919bd3afed43b7bf97c991b
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-03-12 14:59:43 -0500

    Finished Part4

--- DNA Exercises/StringsFirstAssigments/Part4.java
+++ DNA Exercises/StringsFirstAssigments/Part4.java
@@ -10,13 +10,10 @@ public class Part4 {
     public static void main (String[] args) {
         URLResource ur = new URLResource("http://www.dukelearntoprogram.com/course2/data/manylinks.html");
         int c = 0;
-        String tmp;
         for(String s : ur.words()) {
-            tmp = s;
-            s = s.toLowerCase();
-            int yTubeIndex = s.indexOf("youtube");
+            String tmp = s.toLowerCase();
+            int yTubeIndex = tmp.indexOf("youtube");
             if( yTubeIndex != -1) {
-                s = tmp;
                 int leftDoubleQ = s.lastIndexOf("\"", yTubeIndex);
                 int rightDoubleQ = s.indexOf("\"", yTubeIndex);
                 System.out.println(s.substring(leftDoubleQ + 1, rightDoubleQ));
