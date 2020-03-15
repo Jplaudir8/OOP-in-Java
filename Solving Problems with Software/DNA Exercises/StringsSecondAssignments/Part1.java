675af6ff7419406b5357c2b1ddbef02afbec7fb7
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-03-14 22:38:04 -0500

    further improvement in new project

---
+++ Solving Problems with Software/DNA Exercises/StringsSecondAssignments/Part1.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of Part1 here.
+ * 
+ * @author Joan Perez Lozano 
+ */
+public class Part1 {
+    public int findStopCodon(String dna, int startIndex, String stopCodon){
+        return 0;
+    }
+    
+    public void testFindStopCodon(){
+    }
+    
+    public String findGene(String dna){
+        return "";
+    }
+    
+    public void testFindGene(){
+    }
+    
+    public void printAllGenes(String dna){}
+}
