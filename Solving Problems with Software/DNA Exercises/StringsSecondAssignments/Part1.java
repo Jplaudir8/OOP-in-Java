43875de572487e8ee2dd4acb2c7b4e9c95fb551a
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-03-16 22:52:34 -0500

    findStopCodon Finished

--- Solving Problems with Software/DNA Exercises/StringsSecondAssignments/Part1.java
+++ Solving Problems with Software/DNA Exercises/StringsSecondAssignments/Part1.java
@@ -6,10 +6,27 @@
  */
 public class Part1 {
     public int findStopCodon(String dna, int startIndex, String stopCodon){
-        return 0;
+        int currIndex = dna.indexOf(stopCodon, startIndex + 3);
+        while(currIndex != -1){
+            int diff = currIndex - startIndex;
+            if(diff % 3 == 0){
+                return currIndex;
+            } else {
+                currIndex = dna.indexOf(stopCodon, currIndex + 1);
+            }
+        }
+        return dna.length();
     }
     
     public void testFindStopCodon(){
+        String case1 = "ATGXXXYYYZZZTAA"; // Contains Gene at 1st Iteration
+        String case2 = "ATGXXXYYZZZTGAXXXXTAA"; // Contains Gene at 2nd Iteration
+        String case3 = "ATGXXXYYZZZTGAXXXXTTA"; // Does not contain any gene
+        
+        findStopCodon(case1, 0, "TAA");
+        findStopCodon(case2, 0, "TAA");
+        findStopCodon(case3, 0, "TAA");
+        
     }
     
     public String findGene(String dna){
