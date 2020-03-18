8aed4a4f539fe0fea0682466043183d856e50964
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-03-17 19:14:32 -0500

    typo

--- Solving Problems with Software/DNA Exercises/StringsSecondAssignments/Part1.java
+++ Solving Problems with Software/DNA Exercises/StringsSecondAssignments/Part1.java
@@ -37,7 +37,7 @@ public class Part1 {
     
     public String findGene(String dna){
         int startIndex = dna.indexOf("ATG");
-        if (startIndex == -1) return ""; // 1st occurence of ATG, if not then return ""
+        if (startIndex == -1) return ""; // 1st occurence of ATG, if not found then return ""
         int taaIndex = findStopCodon(dna, startIndex, "TAA");
         int tagIndex = findStopCodon(dna, startIndex, "TAG");
         int tgaIndex = findStopCodon(dna, startIndex, "TGA");
