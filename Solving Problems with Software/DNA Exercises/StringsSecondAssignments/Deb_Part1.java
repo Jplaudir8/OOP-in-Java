1b05f394d22cc2fd2da3ef81e0efc93b2e093b75
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-03-19 22:24:09 -0500

    Finished part2 in Deb_Part1 class

--- Solving Problems with Software/DNA Exercises/StringsSecondAssignments/Deb_Part1.java
+++ Solving Problems with Software/DNA Exercises/StringsSecondAssignments/Deb_Part1.java
@@ -5,22 +5,23 @@
  * @author Joan Perez Lozano
  */
 public class Deb_Part1 {
-   public void findAbc(String input) {
+   public void findAbc(String input){
        int index = input.indexOf("abc");
-        while (true) {
-            if (index == -1) {
-                break;
-            }
-            String found = input.substring(index+1, index+4);
-            System.out.println(index+1);
-            System.out.println(index+4);
-            //System.out.println(found);
-            index = input.indexOf("abc", index+4);
+       while (true){
+           if (index == -1 || index >= input.length() - 3){
+               break;
+           }
+           System.out.println("Index 1: " + index);
+           String found = input.substring(index+1, index+4);
+           System.out.println("Index 2: " + index);
+           System.out.println(found);
+           index = input.indexOf("abc",index+3);
+           System.out.println("Index 3: " + index);
        }
    }
-   
-   public void test() {
+
+   public void test(){
        //findAbc("abcd");
-       findAbc("abcdabc");
+       findAbc("qwertyabcuioabcp");
    }
 }
