25fc0c654ce6424e43492e7d31429738122ed2bf
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-03-21 17:31:46 -0500

    Finished Part2 class

--- Solving Problems with Software/DNA Exercises/StringsThirdAssignments/Part2.java
+++ Solving Problems with Software/DNA Exercises/StringsThirdAssignments/Part2.java
@@ -6,7 +6,53 @@
  */
 public class Part2 {
     public double cgRatio(String dna){
+        float cgCount = 0;
+        int index = 0;
+        int startIndex = 0;
         
-        return 0.0;
+        dna = dna.toLowerCase();
+        
+        while(true) {
+            int pos = dna.indexOf("c", startIndex);
+            if (pos == -1) {
+                startIndex = 0;
+                break;
+            }
+            cgCount++;
+            startIndex = pos + 1;
+        }
+        
+        while(true) {
+            int pos = dna.indexOf("g", startIndex);
+            if (pos == -1) {
+                startIndex = 0;
+                break;
+            }
+            cgCount++;
+            startIndex = pos + 1;
+        }
+        
+        return (double) cgCount/dna.length();
+    }
+    
+    // public void testCgRatio(){
+        
+        // String case1 = "ATGCCATAG";
+        // System.out.println("The Ratio is: " + cgRatio(case1));
+    // }
+    
+    public int countCTG(String dna) {
+        dna = dna.toLowerCase();
+        int startIndex = 0;
+        int countCTG = 0;
+        
+        while(true) {
+            int pos = dna.indexOf("ctg", startIndex);
+            if(pos == -1) break;
+            countCTG++;
+            startIndex = pos + 1;
+        }
+        
+        return countCTG;
     }
 }
