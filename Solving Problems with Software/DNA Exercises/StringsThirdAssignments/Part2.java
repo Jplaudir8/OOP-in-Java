5f0d0beb734dbad009ee2d61e4a7440517073dba
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-03-20 23:16:46 -0500

    Started Part2

---
+++ Solving Problems with Software/DNA Exercises/StringsThirdAssignments/Part2.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of Part2 here.
+ * 
+ * @author Joan Perez Lozano
+ */
+public class Part2 {
+    public double cgRatio(String dna){
+        
+        return 0.0;
+    }
+}
