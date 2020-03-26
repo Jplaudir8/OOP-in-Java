ec9745f3d439d641eee44bf3d3f70b11938d0a0f
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-03-25 22:29:58 -0500

    Finished part 1

--- Solving Problems with Software/Manipulating CSV Files/ColdestTemperatureExercise/CSVColdest.java
+++ Solving Problems with Software/Manipulating CSV Files/ColdestTemperatureExercise/CSVColdest.java
@@ -2,9 +2,46 @@
 /**
  * Write a description of CSVColdest here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Joan Perez Lozano
  */
-public class CSVColdest {
 
+import edu.duke.*;
+import org.apache.commons.csv.*;
+import java.io.*;
+
+public class CSVColdest {
+    public CSVRecord coldestHourInFile(CSVParser parser) {
+        CSVRecord lowestSoFar = null;
+        for(CSVRecord currentRow: parser){
+            if(Double.parseDouble(currentRow.get("TemperatureF")) != -9999 ){
+                lowestSoFar = getLowestSoFar(currentRow, lowestSoFar);
+            }
+        }
+        return null;
+    }
+    
+    public CSVRecord getLowestSoFar(CSVRecord currentRow, CSVRecord lowestSoFar) {
+        if(lowestSoFar == null){
+            lowestSoFar = currentRow;
+        } else {
+            double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
+            double lowestTemp = Double.parseDouble(lowestSoFar.get("TemperatureF"));
+            if(currentTemp > lowestTemp){
+                lowestSoFar = currentRow;
+            }
+        }
+        return lowestSoFar;
+    }
+    
+    public void testColdestHourInFile() {
+        FileResource fr = new FileResource();
+        CSVParser parser = fr.getCSVParser();
+        CSVRecord lowest = coldestHourInFile(parser);
+        System.out.println("hottest temperature was " + lowest.get("TemperatureF") +
+                            " at " + lowest.get("TimeEST"));
+    }
+    
+    
 }
+
+
