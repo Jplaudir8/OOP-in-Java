3b660728c3a40d19b1c28bdaaa2d6fceb3c80ae6
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-04-05 19:40:15 -0500

    added encrypt function

--- Arrays, Lists, and Structured Data/CaesarCipher/CaesarCipher.java
+++ Arrays, Lists, and Structured Data/CaesarCipher/CaesarCipher.java
@@ -2,9 +2,32 @@
 /**
  * Write a description of CaesarCipher here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Joan Perez Lozano
  */
-public class CaesarCipher {
 
+public class CaesarCipher {
+    
+    public String encrypt(String input, int key) {
+        StringBuilder encrypted = new StringBuilder(input);
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        // Computing the shifted alphabet
+        String shiftedAlphabet = alphabet.substring(key) + alphabet.substring(0, key);
+        
+        System.out.println(alphabet);
+        System.out.println(shiftedAlphabet);
+        
+        for (int i = 0; i < encrypted.length(); i++) {
+            char currChar = encrypted.charAt(i);
+            int idx = alphabet.indexOf(currChar);
+            
+            if(idx != -1) {
+                char newChar = shiftedAlphabet.charAt(idx);
+                encrypted.setCharAt(i, newChar);
+            }
+        }
+        
+        return encrypted.toString();
+    }
+    
+    
 }
