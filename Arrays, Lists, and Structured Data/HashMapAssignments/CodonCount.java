7d5dadc7cde81cfb01a7ba0c7438f7f10ab35319
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-04-26 11:31:34 -0500

    added getMostCommonCodon and documentation

--- Arrays, Lists, and Structured Data/HashMapAssignments/CodonCount.java
+++ Arrays, Lists, and Structured Data/HashMapAssignments/CodonCount.java
@@ -8,31 +8,70 @@ import edu.duke.*;
 import java.util.*;
 
 public class CodonCount {
-    private HashMap<String, Integer> map;
+    /**
+     * These field or instance variable is a HashMap that will 
+     * store all codons and their ocurrences found in a strand of DNA.
+     */
+    private HashMap<String, Integer> codonMap;
     
+    /**
+     * Initialize codonMap
+     */
     public CodonCount() {
-        map = new HashMap<String, Integer>();
+        codonMap = new HashMap<String, Integer>();
     }
     
+    /**
+     * Register all codons found and their ocurrences in the HashMap.
+     * 
+     * @param  start    the index at which the function starts reading
+     *                  for the codons.
+     * @param  dna      the strand of DNA that will be read.
+     */
     public void buildCodonMap(int start, String dna){
-        map.clear();
+        codonMap.clear();
         int codons = (dna.length()-start)/3;
         String currentCodon;
         for (int i = 0; i <= codons-1; i++) {
-            currentCodon = dna.substring((i*3) + start, (i*3)+start+3);
-            if(!map.containsKey(currentCodon)) {
-                map.put(currentCodon, 1);
+            currentCodon = dna.substring((i*3)+start, (i*3)+start + 3);
+            if(!codonMap.containsKey(currentCodon)) {
+                codonMap.put(currentCodon, 1);
             } else {
-                map.put(currentCodon, map.get(currentCodon) + 1);
+                codonMap.put(currentCodon, codonMap.get(currentCodon) + 1);
             }
         }
-        
-        System.out.println("Codons in list");
-        for(String s : map.keySet()){
-            System.out.println(s + " " + map.get(s));
-        }
-        System.out.println(map.size());
+        // System.out.println("Codons in list");
+        // for(String s : map.keySet()){
+            // System.out.println(s + " " + map.get(s));
+        // }
+        // System.out.println(map.size());
     }
+    
+    /**
+     * Compute the codon that has the most ocurrences and return it. 
+     * If more than one has the same ocurrences, return any.
+     * 
+     * @return  the codon found.
+     */
+    public String getMostCommonCodon() {
+        String mostCodon = "";
+        int mostOcurrence = 0; // Most occurrence of variable 'mostCodon'
+        for(String s : codonMap.keySet()) {
+            
+            if(mostCodon.length() == 0){
+                mostCodon = s;
+            }
+            
+            if(codonMap.get(mostCodon) < codonMap.get(s)){
+                mostOcurrence = codonMap.get(s);
+                mostCodon = s;
+            }
+        }
+        return mostCodon;
+    }
+    
+    
+    
     
     
     
