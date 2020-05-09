56021888ae1bcb71c9f6879453ab934031d8c652
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-05-08 23:54:24 -0500

    added functions to VigenereBreaker

--- Arrays, Lists, and Structured Data/VigenereProgram/VigenereBreaker.java
+++ Arrays, Lists, and Structured Data/VigenereProgram/VigenereBreaker.java
@@ -35,6 +35,83 @@ public class VigenereBreaker {
         System.out.println("Array of Keys: ");
         for(int key : keys) System.out.println(key);
         System.out.println("Decrypted Message: \n" + decryptedMsg);
-        
     }
+    
+    /**
+     * Read list of words from a FileResource file.
+     * 
+     * @param   fr      file that will be read. Should have one words per line.
+     * @return          HashSet of words that have been read from the file,
+     *                  also making sure that every word is in lowercase.
+     */
+    public HashSet<String> readDictionary(FileResource fr) {
+        HashSet<String> words = new HashSet<String>();
+        for(String word : fr.lines()){
+            word = word.toLowerCase();
+            words.add(word);
+        }
+        return words;
+    }
+    
+    /**
+     * Count how many of these possible words are contained in the dictionary.
+     * 
+     * @param   message
+     */
+    public int countWords(String message, HashSet<String> dictionary) {
+        int count = 0;
+        String[] words = message.split("\\W");
+
+        for(String word : words){
+            if(dictionary.contains(word)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public String breakForLanguage(String encrypted, HashSet<String> dictionary) {
+        // Here we are just testing a range from 1 to 100 as possible values
+        // for the keyLength. There is nothing special about 100, we could
+        // also iterate until encrypted.length()
+        HashMap<int[], Integer> mapKeysOcurrences = new HashMap<int[], Integer>();
+        
+        for(int i = 1; i <= 100; i++) {
+            int[] keys = tryKeyLength(encrypted, i, 'e');
+            VigenereCipher vc = new VigenereCipher(keys);
+            String decrypted = vc.decrypt(encrypted);
+            FileResource dictionaryFile = new FileResource("dictionaries/english");
+            HashSet<String> dictionarySet = readDictionary(dictionaryFile);
+            int ocurrencesKey = countWords(decrypted, dictionarySet);
+            mapKeysOcurrences.put(keys, ocurrencesKey);
+        }
+        
+        int maxOcurrences = 0;
+        int[] decisiveKeys = null;
+        
+        for(int[] keys : mapKeysOcurrences.keySet()) {
+            if(mapKeysOcurrences.get(keys) > maxOcurrences) {
+                maxOcurrences = mapKeysOcurrences.get(keys);
+                decisiveKeys = keys;
+            }
+        }
+        
+        VigenereCipher vc = new VigenereCipher(decisiveKeys);
+        String decrypted = vc.decrypt(encrypted);
+        
+        return decrypted;
+    }    
+    
+
+
+
+
+
+
+
+
+
+
+
+
 }
