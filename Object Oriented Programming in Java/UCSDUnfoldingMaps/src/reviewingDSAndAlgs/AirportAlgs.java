48230410c776eeabdfe527e7ddfe7f61cbc8a975
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-06-21 20:08:36 -0500

    added comments and binary search

--- Object Oriented Programming in Java/UCSDUnfoldingMaps/src/reviewingDSAndAlgs/AirportAlgs.java
+++ Object Oriented Programming in Java/UCSDUnfoldingMaps/src/reviewingDSAndAlgs/AirportAlgs.java
@@ -3,11 +3,10 @@ package reviewingDSAndAlgs;
 public class AirportAlgs {
 
 
-
-
-
-	// Performing a basic linear search to find airport code
-	public static String findAirportCode(String toFind, Airport[] airports) {
+	// Algorithm 1
+	// Performing a basic linear search to find airport code.
+	// Computation here would be n times for n number of inputs.
+	public static String findAirportCode1(String toFind, Airport[] airports) {
 		int index = 0;
 		while(index < airports.length){
 			Airport a = airports[index];
@@ -16,7 +15,38 @@ public class AirportAlgs {
 			}
 			index++;
 		}
-		
-		return "NOT FOUND";
+		return null;
 	}
+	
+	// Algorithm 2
+	// Improving Algorithm 1 by performing a binary search to find the airport code.
+	// Computation here would be expressed as log(n) for n number of inputs.
+	// It is also good to be aware that this implementation of low and high variables 
+	// may fall in a situation in which we may be dealing with very huge indexes, 
+	// making it not possible to represent an addition of the two numbers(also called as overflow). 
+	// In order to help that situation, I decided to use mid = low + ((high-low)/2);
+	public static String findAirportCode2(String toFind, Airport[] airports) {
+		// Initializing local variables expressing a range boundary for the indexes. 
+		// mid variable which will be used to start cutting our search in halves.
+		int mid, low = 0, high = airports.length-1;
+		while(low <= high) {
+			mid = low + ((high-low)/2);
+			int compare = toFind.compareTo(airports[mid].getCity());
+			if(compare < 0) {
+				high = mid - 1;
+			} else if(compare > 0) {
+				low = mid + 1;
+			} else {
+				return airports[mid].getCity();
+			}
+		}
+		return null;
+	}
+	
+	
+	
+	
+	
+	
+	
 }
