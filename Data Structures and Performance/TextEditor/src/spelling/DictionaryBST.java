cd78e46aa72199b8072f674bb5c40be0dafe0ddf
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-07-28 18:05:01 -0500

    implemented Dictionary BST class

--- Data Structures and Performance/TextEditor/src/spelling/DictionaryBST.java
+++ Data Structures and Performance/TextEditor/src/spelling/DictionaryBST.java
@@ -10,8 +10,9 @@ public class DictionaryBST implements Dictionary
 {
    private TreeSet<String> dict;
 	
-    // TODO: Implement the dictionary interface using a TreeSet.  
- 	// You'll need a constructor here
+    public DictionaryBST() {
+    	dict = new TreeSet<>();
+    }
 	
     
     /** Add this word to the dictionary.  Convert it to lowercase first
@@ -20,7 +21,11 @@ public class DictionaryBST implements Dictionary
      * @return true if the word was added to the dictionary 
      * (it wasn't already there). */
     public boolean addWord(String word) {
-    	// TODO: Implement this method
+    	word = word.toLowerCase();
+    	if (!dict.contains(word)) {
+    		dict.add(word);
+    		return true;
+    	}
         return false;
     }
 
@@ -28,14 +33,12 @@ public class DictionaryBST implements Dictionary
     /** Return the number of words in the dictionary */
     public int size()
     {
-    	// TODO: Implement this method
-        return 0;
+        return dict.size();
     }
 
     /** Is this a word according to this dictionary? */
-    public boolean isWord(String s) {
-    	//TODO: Implement this method
-        return false;
+    public boolean isWord(String s) {    	
+        return dict.contains(s.toLowerCase());
     }
 
 }
